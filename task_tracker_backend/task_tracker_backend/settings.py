ce23aae511248131a2a70b032a8f4ccf2c37a736
Josh Hill <joshuaamhill@gmail.com>
2024-12-04 15:01:46 -0800

    WIP: Render deploy

--- task_tracker_backend/task_tracker_backend/settings.py
+++ task_tracker_backend/task_tracker_backend/settings.py
@@ -50,10 +50,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
